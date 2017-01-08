203120f01fb653fe331881bfdde8cb98143b1f41
ArchanaS <archana.june@gmail.com>
2017-01-08 15:16:06 -0800

    Blog information added

--- content/myblog.js
+++ content/myblog.js
@@ -1,22 +1,30 @@
-var myblog = [
+var blogData = [
   {
-    title: '',
-    description: '',
-    link: ''
+    title: 'Build complex layouts with postcss-flexbox',
+    tags: '#postcss-flexbox #flexbox #fancy-layouts-easy',
+    date: 'Nov 11',
+    year: '2016',
+    link: 'http://www.creativebloq.com/features/5-top-flexbox-and-postcss-resources'
   },
   {
     title: 'Mindful CSS with PostCSS',
-    description: '',
+    tags: '#postcss #colorguard #CSSperf',
+    date: 'Feb 4',
+    year: '2016',
     link: 'https://medium.com/@archana_june/mindful-css-with-postcss-b0ad3fde018a#.c0ap4ae4q'
   },
   {
-    title: 'Women in Tech',
-    description: '',
-    link: ''
+    title: 'Emerging Leaders in Engineering',
+    tags: '#techwomen #leaders',
+    date: 'Nov 5',
+    year: '2015',
+    link: 'https://medium.com/@archana_june/emerging-leaders-in-engineering-b5e3947c8d78#.k426qcbcy'
   },
   {
-    title: 'Introduction to CSS3 Flexbox',
-    description: '',
-    link: ''
+    title: 'Flexbox at Coursera',
+    tags: '#flexbox #ready-made-classes',
+    date: 'June 23',
+    year: '2015',
+    link: 'https://building.coursera.org/blog/2015/06/23/flexbox-at-work/'
   }
 ];
