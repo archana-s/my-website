6a113dc888303558eb1b18aadd9440458cda86e9
ArchanaS <archana.june@gmail.com>
2017-01-15 14:48:48 -0800

    Final set of changes

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ var app = express()
 var path = require('path')
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
-app.use('/web-components', express.static(path.join(__dirname, '/web-components')))
-app.use('/assets', express.static(path.join(__dirname, '/assets')))
-app.use('/content', express.static(path.join(__dirname, '/content')))
+app.use('/public', express.static(path.join(__dirname, '/public')))
