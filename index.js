7f3d7550430beb4a39c21521f3a62ac4c7b64a8c
ArchanaS <archana.june@gmail.com>
2017-01-15 17:00:49 -0800

    Add a new profile image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 var path = require('path')
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(3000, function () {
