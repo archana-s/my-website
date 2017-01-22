d05d8a9caebf5c048f10669d997db308c1d4c105
ArchanaS <archana.june@gmail.com>
2017-01-21 20:37:27 -0800

    Adding routes to enable the rest of the routes with reload

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var path = require('path')
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
+app.get('/:page', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
